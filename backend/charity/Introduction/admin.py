c409cb700c2f2a805187a391505ed94423e361d9
delta <r.moosavi1381@gmail.com>
2023-06-24 19:30:17 +0330

    ignore some files

--- backend/charity/Introduction/admin.py
+++ backend/charity/Introduction/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Vision
+
+
+
+@admin.register(Vision)
+class VisionAdmin(admin.ModelAdmin):
+    list_display = ['title','last_update']
+    
+    def has_add_permission(self, request, obj=None):
+        # Disable the 'Add' button
+        return False
+    
+
+
+
+
