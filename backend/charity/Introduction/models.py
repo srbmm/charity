847531bae5e53191e87865a4a38200a0666b9491
delta <r.moosavi1381@gmail.com>
2023-06-24 18:49:16 +0330

    add the urls for introduction app and vision model class

--- backend/charity/Introduction/models.py
+++ backend/charity/Introduction/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
 # Create your models here.
+
+class SingletonModelManager(models.Manager):
+    def create(self, **kwargs):
+        
+        if self.model.objects.exists():
+            return self.model.objects.get()
+        return super().create(**kwargs)
+
+    
+    
+
+
+
+
+class Vision(models.Model):
+    title = models.CharField(max_length=50)
+    avatar = models.ImageField(blank=True,upload_to='Introduction/')
+    text = models.TextField()
+    last_update = models.DateTimeField('last update',auto_now=True)
+
+
+    class Meta:
+        db_table = 'Vision'
+        verbose_name = 'Vision'
+        verbose_name_plural = 'Vision'
+
+
+
+    objects = SingletonModelManager()
+
+    def save(self, *args, **kwargs):
+        if Vision.objects.exists() and not self.pk:
+            self.pk = 1
+        super().save(*args, **kwargs)
+    
+    def delete(self, *args, **kwargs):
+        # Prevent deletion of the only instance
+        pass
+
+
+
+    
+
+
