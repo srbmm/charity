8dec9eade2d1d070b7a2f90c0f208be4a481abf2
delta <r.moosavi1381@gmail.com>
2023-07-03 19:00:13 +0330

    add the support topics list view

--- backend/charity/projects/urls.py
+++ backend/charity/projects/urls.py
@@ -1,11 +1,12 @@
 
 
 from django.urls import path
-
+from .views import SupportTopicsListView
 
 
 urlpatterns = [
-    
+    path('all/',SupportTopicsListView.as_view(),name='support topics'),
+
 ]
 
 
--- backend/charity/projects/views.py
+++ backend/charity/projects/views.py
@@ -18,7 +18,25 @@ class SupportTopicsListView(APIView):
 
 
     def get(self,request):
-        pass
+
+        topic = None
+
+        if(len(request.query_params) == 0):
+            all_the_objects = SupportTopics.objects.all()
+            serializer = SupportTopicsSerializer(all_the_objects , many=True ,context = {'request' : request})
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        
+
+
+        if request.query_params.get('topic'):
+            topic = request.query_params.get('topic')
+            all_the_objects = SupportTopics.objects.filter(topic = topic)
+            serializer = SupportTopicsSerializer(all_the_objects , many=True ,context = {'request' : request})
+            return Response(serializer.data,status=status.HTTP_200_OK)
+
+
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 
