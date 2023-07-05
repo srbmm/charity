8401177508449e0ad47bfc1efeee5e8d84e487f9
delta <r.moosavi1381@gmail.com>
2023-07-05 07:25:45 +0330

    rename the urls

--- backend/charity/charity/urls.py
+++ backend/charity/charity/urls.py
@@ -12,12 +12,12 @@ from django.conf.urls.static import static
 urlpatterns = [
     
     path('admin/', admin.site.urls),
-    path('News/',include('News.urls')),
-    path('Introduction/',include('Introduction.urls')),
-    path("Members/",include("Members.urls")),
-    path('SupportTopics/',include('SupportTopics.urls')),
-    path('Supporters/',include('Supporters.urls')),
-    path('Projects/',include('Project.urls')),
+    path('news/',include('News.urls')),
+    path('introduction/',include('Introduction.urls')),
+    path("members/",include("Members.urls")),
+    path('supportTopics/',include('SupportTopics.urls')),
+    path('supporters/',include('Supporters.urls')),
+    path('projects/',include('Project.urls')),
     path('Info/',include('Information.urls')),
 
 
