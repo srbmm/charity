ae2a5f77e6cd87048c9e48db932b4d8d7a5a4a3b
delta <r.moosavi1381@gmail.com>
2023-07-05 15:01:23 +0330

    check the verbose name in admin

--- backend/charity/News/models.py
+++ backend/charity/News/models.py
@@ -4,17 +4,17 @@ from django.db import models
 
 
 class News(models.Model):
-    title = models.CharField(max_length=50)
-    avatar = models.ImageField(blank=True,upload_to='News/')
-    text = models.TextField(blank=True)
-    created_date = models.DateTimeField('created date',auto_now_add=True)
-    summary = models.TextField(blank=True)
-    seen_count = models.IntegerField(default=0)
-    is_selected = models.BooleanField(default=False)
+    title = models.CharField(max_length=50,verbose_name='عنوان')
+    avatar = models.ImageField(blank=True,upload_to='News/',verbose_name='تصویر خبر')
+    text = models.TextField(blank=True,verbose_name='متن خبر')
+    created_date = models.DateTimeField(auto_now_add=True,verbose_name='تاریخ ساخت خبر')
+    summary = models.TextField(blank=True,verbose_name='خلاصه خبر')
+    seen_count = models.IntegerField(default=0,verbose_name='تعداد بازدید')
+    is_selected = models.BooleanField(default=False,verbose_name='خبر خاص')
 
 
 
     class Meta:
         db_table = 'News'
-        verbose_name = 'News'
-        verbose_name_plural = 'News' 
+        verbose_name = 'خبر'
+        verbose_name_plural = 'اخبار' 
