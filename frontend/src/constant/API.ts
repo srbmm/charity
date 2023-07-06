b38ad4441304ff0394b924217d7bf3a68b64ab75
Mohammad Sohrabi <shrbmamad@gmail.com>
2023-07-06 08:06:13 +0330

    change urls

--- frontend/src/constant/API.ts
+++ frontend/src/constant/API.ts
@@ -1,26 +1,26 @@
 const ADDRESS = "http://127.0.0.1:8000"
-const NEWS = ADDRESS + "/News"
+const NEWS = ADDRESS + "/news"
 const NEWS_CHOICES = NEWS
 const INTRODUCTION = {
-    "vision" : ADDRESS + "/Introduction/vision",
-    "history" : ADDRESS + "/Introduction/history",
-    "mission" : ADDRESS + "/Introduction/mission",
-    "chart" : ADDRESS + "/Introduction/chart",
-    "statute" : ADDRESS + "/Introduction/statute",
-    "values" : ADDRESS + "/Introduction/values",
-    "permissions" : ADDRESS + "/Introduction/permissions",
-    "audit" : ADDRESS + "/Introduction/reports/audit",
-    "performance" : ADDRESS + "/Introduction/reports/performance",
-    "financial" : ADDRESS + "/Introduction/reports/financial",
-    "faq" : ADDRESS + "/Introduction/faq",
+    "vision" : ADDRESS + "/introduction/vision",
+    "history" : ADDRESS + "/introduction/history",
+    "mission" : ADDRESS + "/introduction/mission",
+    "chart" : ADDRESS + "/introduction/chart",
+    "statute" : ADDRESS + "/introduction/statute",
+    "values" : ADDRESS + "/introduction/values",
+    "permissions" : ADDRESS + "/introduction/permissions",
+    "audit" : ADDRESS + "/introduction/reports/audit",
+    "performance" : ADDRESS + "/introduction/reports/performance",
+    "financial" : ADDRESS + "/introduction/reports/financial",
+    "faq" : ADDRESS + "/introduction/faq",
 }
 
 const MEMBERS = {
-    "previousـboardـmembers" : ADDRESS + "/Members/previous-board",
-    "currentـboardـmembers" : ADDRESS + "/Members/ current-board",
-    "members_of_the_board_of_trustees" : ADDRESS + "/Members/trustees",
-    "members_of_the_founding_board" : ADDRESS + "/Members/founders",
-    "investigators" : ADDRESS + "/Members/inspectors",
-    "members_of_the_general_assembly" : ADDRESS + "/Members/members",
+    "previousـboardـmembers" : ADDRESS + "/members/previous-board",
+    "currentـboardـmembers" : ADDRESS + "/members/ current-board",
+    "members_of_the_board_of_trustees" : ADDRESS + "/members/trustees",
+    "members_of_the_founding_board" : ADDRESS + "/members/founders",
+    "investigators" : ADDRESS + "/members/inspectors",
+    "members_of_the_general_assembly" : ADDRESS + "/members/members",
 }
 export {NEWS, INTRODUCTION, MEMBERS}
