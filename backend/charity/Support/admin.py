739fc5f989acf96e892215b308dc6030d9827bf1
delta <r.moosavi1381@gmail.com>
2023-07-21 18:59:00 +0330

    edit support id in admin

--- backend/charity/Support/admin.py
+++ backend/charity/Support/admin.py
@@ -5,7 +5,7 @@ from .models import SupportReceipt,NonCashSupport
 
 @admin.register(SupportReceipt)
 class SupportReceiptAdmin(admin.ModelAdmin):
-    exclude =('support_date',)
+    
     list_display = [
         'full_name',
         'amount'
@@ -13,7 +13,7 @@ class SupportReceiptAdmin(admin.ModelAdmin):
     list_filter = ['topic','support_fund']
 
     def get_readonly_fields(self, request, obj=None):
-        return [field.name for field in self.opts.fields]
+        return [field.name for field in self.opts.fields if field.name != 'id']
 
     def has_add_permission(self, request,object = None) -> bool:
         return False
@@ -21,7 +21,7 @@ class SupportReceiptAdmin(admin.ModelAdmin):
 
 @admin.register(NonCashSupport)
 class NonCashSupportAdmin(admin.ModelAdmin):
-    exclude =('support_date',)
+    exclude =('id','support_date')
     list_display = [
         'full_name',
         'support_type',
@@ -30,7 +30,7 @@ class NonCashSupportAdmin(admin.ModelAdmin):
     list_filter = ['topic','support_type']
 
     def get_readonly_fields(self, request, obj=None):
-        return [field.name for field in self.opts.fields]
+        return [field.name for field in self.opts.fields if field.name != 'id']
 
     def has_add_permission(self, request,object = None) -> bool:
         return False
--- backend/charity/Support/models.py
+++ backend/charity/Support/models.py
@@ -18,10 +18,18 @@ class NonCashSupport(models.Model):
         ('expertise','تخصص'),
     ]
 
-    topic = models.CharField(max_length=20,choices=TOPICS_CHOICES,verbose_name='محل مصرف')
+    topic = models.CharField(
+            max_length=20,choices=TOPICS_CHOICES,
+            verbose_name='محل مصرف',blank=True,null=True,
+            default=None
+          )
     full_name = models.CharField(max_length=255,verbose_name='نام و نام خانوادگی')
     phone_number =  models.CharField(max_length=20,verbose_name='تلفن همراه')
-    support_type = models.CharField(max_length=40,choices=SUPPORT_TYPE_CHOICES,verbose_name='نوع حمایت')
+    support_type = models.CharField(
+            max_length=40,choices=SUPPORT_TYPE_CHOICES,
+            verbose_name='نوع حمایت',
+            blank=True,null=True,default=None  
+        )
     description = models.TextField(verbose_name='توضیحات')
     support_date = models.DateField(verbose_name='تاریخ حمایت',auto_now_add=True)
     
@@ -31,7 +39,7 @@ class NonCashSupport(models.Model):
         verbose_name_plural = 'حمایت های غیر نقدی'
 
     def __str__(self):
-        return f' {self.full_name}         {self.support_type}'
+        return f' {self.full_name} '
 
 
 class SupportReceipt(models.Model):
@@ -50,8 +58,16 @@ class SupportReceipt(models.Model):
         ('social','فعالیت های اجتماعی و فوق برنامه'),
     ]
 
-    topic = models.CharField(max_length=20,choices=TOPICS_CHOICES,verbose_name='موضوع')
-    support_fund = models.CharField(max_length=70,choices=SUPPORT_FUND_CHOICES,verbose_name='صندوق حمایت')
+    topic = models.CharField(
+            max_length=20,choices=TOPICS_CHOICES,
+            verbose_name='موضوع',blank=True,
+            null=True,default=None
+        )
+    support_fund = models.CharField(
+            max_length=70,choices=SUPPORT_FUND_CHOICES,
+           verbose_name='صندوق حمایت',blank=True,
+           null=True,default=None
+        )
     full_name = models.CharField(max_length=255,verbose_name='نام و نام خانوادگی')
     email = models.EmailField(verbose_name='ایمیل',blank=True,null=True)
     phone_number = models.CharField(max_length=20,verbose_name='تلفن همراه')
@@ -64,5 +80,5 @@ class SupportReceipt(models.Model):
         verbose_name_plural = 'صندوق حمایت ها'
 
     def __str__(self):
-        return f' {self.full_name}         {self.amount}'
+        return f' {self.full_name}'
     
